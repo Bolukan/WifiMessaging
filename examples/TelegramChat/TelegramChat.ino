e5145982bcf7d463f9fdccb07bb193d25e61e979
unknown <github@bolukan.nl>
2020-08-30 16:03:10 +0200

    Add example

---
+++ examples/TelegramChat/TelegramChat.ino
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <wifimessaging.h>
+
+void callback(char *topic, byte *payload, unsigned int length);
+
+// WiFi and MQTT
+WifiMessaging myWM(WifiMessaging::connectionService::ServiceWifi |
+                       WifiMessaging::connectionService::ServiceMQTT |
+                       WifiMessaging::connectionService::ServiceNTP |
+                       WifiMessaging::connectionService::ServiceSecure |
+                       WifiMessaging::connectionService::ServiceTelegram,
+                   callback);
+
+bool sentMessage = false;
+const char *MQTT_TOPIC = "test/test";
+const char *MQTT_PAYLOAD = "SIGNAL";
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  // DEBUG: Show payload
+  for (unsigned int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  // */
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("\nWifi Messaging test.");
+
+  myWM.connectToWiFi();
+}
+
+void loop()
+{
+  if (
+      (myWM.StatusTelegram == myWM.ConnectionActive) &&
+      (myWM.StatusMQTT == myWM.ConnectionActive) &&
+      (!sentMessage))
+  {
+    myWM.mqttClient.publish(MQTT_TOPIC, MQTT_PAYLOAD);
+    myWM.sendMessage(String(MQTT_PAYLOAD), String(""));
+    sentMessage = true;
+    Serial.println("Message sent. Be proud! (End of messages)");
+  }
+}
